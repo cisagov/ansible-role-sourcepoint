7b2d851c6475e586044b89dd6096db7a6688bc05
hsfetty <hsfetty@gmail.com>
2022-06-29 10:10:07 -0400

    molecule tests for files

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,7 +12,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize("d", ["/tools/SourcePoint"])
+def test_directories(host, d):
+    """Test that the expected directories were created and are not empty."""
+    assert host.file(d).exists
+    assert host.file(d).is_directory
+    assert host.file(d).listdir()
+    assert host.file(d).mode == 0o755
+
+
+@pytest.mark.parametrize(
+    "f",
+    [
+        "/etc/profile.d/golang.sh",
+        "/tools/SourcePoint/SourcePoint.go",
+        "/tools/SourcePoint/SourcePoint",
+    ],
+)
+def test_files(host, f):
+    """Test that the expected files were created and are non-empty."""
+    assert host.file(f).exists
+    assert host.file(f).is_file
+    assert host.file(f).content
